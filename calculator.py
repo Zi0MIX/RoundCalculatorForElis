c9a8473a6793f80d161487224e9ef5740a3ed356
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2022-10-14 00:21:18 +0200

    Full code refactor for release

--- calculator.py
+++ calculator.py
@@ -1,82 +1,12 @@
-def spawn_override(rnd: int) -> float:
-    overrides = {
-        "1": 2,
-        "2": 1.9,
-        "3": 1.8,
-        "4": 1.7,
-        "5": 1.65,
-        "6": 1.55,
-        "7": 1.45,
-        "8": 1.4,
-        "9": 1.35,
-        "10": 1.25,
-        "11": 1.2,
-        "12": 1.15,
-        "13": 1.1,
-        "14": 1.05,
-        "15": 1,
-        "16": 0.95,
-        "17": 0.9,
-        "18": 0.85,
-        "19": 0.8,
-        "20": 0.75,
-        "21": 0.7,
-        "22": 0.7,
-        "23": 0.65,
-        "24": 0.6,
-        "25": 0.6,
-        "26": 0.55,
-        "27": 0.55,
-        "28": 0.5,
-        "29": 0.5,
-        "30": 0.45,
-        "31": 0.45,
-        "32": 0.4,
-        "33": 0.4,
-        "34": 0.35,
-        "35": 0.35,
-        "36": 0.35,
-        "37": 0.3,
-        "38": 0.3,
-        "39": 0.3,
-        "40": 0.25,
-        "41": 0.25,
-        "42": 0.25,
-        "43": 0.25,
-        "44": 0.2,
-        "45": 0.2,
-        "46": 0.2,
-        "47": 0.2,
-        "48": 0.2,
-        "49": 0.15,
-        "50": 0.15,
-        "51": 0.15,
-        "52": 0.15,
-        "53": 0.15,
-        "54": 0.15,
-        "55": 0.15,
-        "56": 0.1,
-        "57": 0.1,
-        "58": 0.1,
-        "59": 0.1,
-        "60": 0.1,
-        "61": 0.1,
-        "62": 0.1,
-        "63": 0.1,
-    }
-
-    return overrides[str(rnd)]
-
-
-def split_input(user_input: str) -> list:
+def split_input(user_input: str) -> list | bool:
     if " " not in user_input:
-        return []
+        return False
 
     raw = str(user_input).split(" ")
     try:
         details = [int(x) for x in raw]
     except ValueError:
-        return []
+        return False
 
     return details
 
@@ -151,7 +81,7 @@ def round_spawn_delay(spawn: float, rnd: int) -> float:
     return spawn
 
 
-def get_spawn_delay(rnd: int) -> float:
+def get_spawn_delay(rnd: int, players: int) -> float:
     zombie_spawn_delay = 2.0
 
     if rnd == 1:
@@ -165,9 +95,10 @@ def get_spawn_delay(rnd: int) -> float:
 
     zombie_spawn_delay = round_spawn_delay(zombie_spawn_delay, rnd)
 
-    # This check should now be obsolete
-    if zombie_spawn_delay < 0.08:
-        zombie_spawn_delay = 0.08
+    if zombie_spawn_delay < 0.2 and players == 1:
+        zombie_spawn_delay = 0.2
+    elif zombie_spawn_delay < 0.15:
+        zombie_spawn_delay = 0.15
 
     return zombie_spawn_delay
 
@@ -178,56 +109,42 @@ def get_network_frame(players: int) -> float:
     return 0.05
 
 
-def print_sequence():
-    network_frame = get_network_frame(1)
-    for x in range(1, 65):
-        zombies = get_zombies(x, 1)
-        spawn_delay = round(get_spawn_delay(x) + network_frame, 2)
-        ts = gmtime(zombies * spawn_delay)
-        nice_result = get_readable_time(ts)
-
-        if len(str(spawn_delay)) == 3:
-            spawn_delay = str(f"{spawn_delay}0")
-        else:
-            spawn_delay = str(spawn_delay)
-        # print(f"{zombies} zombies on round {x} will spawn in {nice_result} with spawnrate of {round(spawn_delay, 2)} (network frame: {network_frame})")
-        print(spawn_delay)
-    return
-
-
 def main():
+    print("Welcome in perfect round times calculator by Zi0.")
+    print("Source: https://github.com/Zi0MIX/ZM-RoundCalculator")
+    print("Enter values separated by spacebar: (round) (players) (range)")
+    print("Round and Players arguments are mandatory, others are optional. Check ARGUMENTS.MD on GitHub for info.")
     while True:
-        print("Enter round, amount of players (example: 100 1)")
         user_input = input("> ")
-        if user_input == "printall":
-            print_sequence()
-            continue
 
         arguments = split_input(user_input)
-        if not isinstance(arguments, list) or len(arguments) != 2:
+        if not isinstance(arguments, list) or len(arguments) < 2:
             continue
 
+        round_range = [arguments[0]]
+        if len(arguments) >= 3 and arguments[2]:
+            round_range = range(1, arguments[0])
+        
         network_frame = get_network_frame(arguments[1])
 
-        zombies = get_zombies(arguments[0], arguments[1])
-        if OVERRIDE_SPAWN:
-            spawn_delay = spawn_override(arguments[0])
-        else:
-            spawn_delay = get_spawn_delay(arguments[0])
+        for rnd in round_range:
+            zombies = get_zombies(rnd, arguments[1])
+            spawn_delay = get_spawn_delay(rnd, arguments[1])
 
-        spawn_delay += network_frame
+            spawn_delay += network_frame
 
-        ts = gmtime(zombies * spawn_delay)
-        nice_result = get_readable_time(ts)
+            ts = gmtime(zombies * spawn_delay)
+            nice_result = get_readable_time(ts)
 
-        if len(str(spawn_delay)) == 3:
-            spawn_delay = str(f"{spawn_delay}0")
+            if len(str(spawn_delay)) == 3:
+                spawn_delay = str(f"{spawn_delay}0")
+            else:
+                spawn_delay = str(round(spawn_delay, 2))
 
-        input(f"{zombies} zombies on round {arguments[0]} will spawn in {nice_result} with spawnrate of {round(spawn_delay, 2)} (network frame: {network_frame})")
+            print(f"{zombies} zombies on round {rnd} will spawn in {nice_result} with spawnrate of {spawn_delay} (network frame: {network_frame})")
 
 
 if __name__ == "__main__":
     CALC_DEBUG = False
-    OVERRIDE_SPAWN = False
     from time import gmtime
     main()
